f6bc8ec2d73eccfd4cde41e26f2395253cb4ce5a
Camila Rovalino <camila.rovalino@gmail.com>
2020-05-28 01:49:36 -0400

    First commit

---
+++ src/Background.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Background.css';
+
+function Background() {
+  return (
+    <div className="sky-container">
+      {/* BACKGROUND STARS & ANIMATIONS */}
+      <div className="stars-0"></div>
+      <div className="stars-1"></div>
+      <div className="stars-2"></div>
+      <div className="stars-3"></div>
+      <div className="stars-4"></div>
+      <div className="stars-5"></div>
+    </div>
+  );
+}
+
+export default Background;
